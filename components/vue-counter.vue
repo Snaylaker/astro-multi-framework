7679b363107402aa148c3f200e2799c5e48ea12d
Snaylaker <mehdihimmid73@gmail.com>
2023-11-21 11:06:46 +0100

    refactor(vue): migrated to declarative api

--- components/vue-counter.vue
+++ components/vue-counter.vue
@@ -1,22 +1,21 @@
-<template>
-	<div>
-		<h2>Vue Counter: {{ count }}</h2>
-		<button @click="increment">Increment</button>
-	</div>
-</template>
+<script setup>
+import { ref, onMounted } from 'vue'
 
-<script>
-export default {
-	data() {
-		return {
-			count: 0,
-		};
-	},
-	methods: {
-		increment() {
-			this.count++;
-		},
-	},
-};
+// reactive state
+const count = ref(0)
+
+// functions that mutate state and trigger updates
+function increment() {
+	count.value++
+}
+
+// lifecycle hooks
+onMounted(() => {
+	console.log(`The initial count is ${count.value}.`)
+})
 </script>
 
+<template>
+	<h2> VueJs Counter: {{ count }} </h2>
+	<button @click="increment">Increment</button>
+</template>
